43ba6e43a796eebaa3909a577e1551a65b73aa54
vicentecondon <vicentecondon@users.noreply.github.com>
2026-07-23 00:41:11 -0300

    App inicial: A quien se lo preste

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'a-quien-se-lo-preste-v1';
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(claves =>
+      Promise.all(claves.filter(c => c !== CACHE).map(c => caches.delete(c)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
